3ae5fafa07b1a93ca5d73be3595b976759a2626c
Casta626 <jacastaneda@iesrafaelalberti.es>
2022-02-03 06:09:53 +0100

    PDF y DB a medias

--- Practica DIN U5/Trabajo 5 DIN/Main.py
+++ Practica DIN U5/Trabajo 5 DIN/Main.py
@@ -1,6 +1,6 @@
 import sys
 from PySide6.QtGui import QPixmap
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWizard, QWizardPage, QLineEdit, QHBoxLayout, QLabel, QComboBox, QTextEdit, QVBoxLayout,QMessageBox
+from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QWizard, QWizardPage, QLineEdit, QHBoxLayout, QLabel, QComboBox, QTextEdit, QVBoxLayout,QMessageBox, QAbstractItemView
 from reportlab.pdfgen.canvas import Canvas
 from pdfrw import PdfReader
 from pdfrw.buildxobj import pagexobj
@@ -9,6 +9,8 @@ from pdfrw.toreportlab import makerl
 import textwrap
 # Para poner la fecha de hoy
 from datetime import datetime
+from PySide6.QtSql import QSqlDatabase, QSqlQuery, QSqlRelation, QSqlRelationalTableModel
+from PySide6.QtCore import Qt
 
 from ui_app import Ui_MainWindow
 class MainWindow(QMainWindow,Ui_MainWindow):
@@ -18,7 +20,53 @@ class MainWindow(QMainWindow,Ui_MainWindow):
         self.setupUi(self)
 
         self.pushButton2.clicked.connect(self.iniciarWizard)
-        
+
+        # Realizamos la carga y apertura de la base de datos
+        db = QSqlDatabase("QSQLITE")
+        db.setDatabaseName("chinook.sqlite")
+
+        db.open()
+        # Creamos una query para obtener todos los artistas de la tabla Artist
+        query = QSqlQuery("SELECT Name FROM Artist",db=db)
+        # Recorremos el resultado de esa query agregando al comboBox el listado de artistas
+        # while query.next():
+        #     self.comboBox_Artist.addItem(query.value(0))
+
+        # Creamos un modelo relacional de SQL
+        self.modelo = QSqlRelationalTableModel(db=db)
+        # Establecemos Album como tabla del modelo
+        self.modelo.setTable("Album")
+        # Establecemos la relación entre el ID de los artistas y su nombre, para que se muestre este último
+        self.modelo.setRelation(2, QSqlRelation("Artist", "ArtistId", "Name"))
+        # Hacemos el select del modelo
+        self.modelo.select()
+        # Renombramos las cabeceras de la tabla
+        self.modelo.setHeaderData(0, Qt.Horizontal, "Id")
+        self.modelo.setHeaderData(1, Qt.Horizontal, "Título")
+        self.modelo.setHeaderData(2, Qt.Horizontal, "Artista")
+
+        # Establecemos el modelo en el tableView
+        self.tableView.setModel(self.modelo)
+        # Ajustamos el tamaño de las columnas al contenido
+        self.tableView.resizeColumnsToContents()
+
+        # Deshabilitamos la edición directa de la tabla
+        self.tableView.setEditTriggers(QAbstractItemView.NoEditTriggers)
+        # Establecemos que se seleccionen filas completas en lugar de celdas individuales
+        self.tableView.setSelectionMode(QAbstractItemView.SingleSelection)
+        self.tableView.setSelectionBehavior(QAbstractItemView.SelectRows)
+
+        # Creamos la señal: Cuando cambie la seleccion, ejecuta self.seleccion
+        # self.tableView.selectionModel().selectionChanged.connect(self.seleccion)
+        # # Creamos la señal: Cuando se ejecute la acción Modificar, ejecuta self.modificar
+        # self.actionModificar.triggered.connect(self.modificar)
+        # # Creamos la señal: Cuando se ejecute la acción Insertar, ejecuta self.nueva
+        # self.actionInsertar.triggered.connect(self.nueva)
+        # # Creamos la señal: Cuando se ejecute la acción Eliminar, ejecuta self.borrar
+        # self.actionEliminar.triggered.connect(self.borrar)
+
+        # Ponemos la fila inicial a un valor que indica que no está seleccionada ninguna fila
+        self.fila = -1
         
 
     def iniciarWizard(self):
